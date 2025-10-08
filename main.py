a6e3d0bfad9a86e0f9183ab2a28504f6ebb51b1a
Ersin Günerigök <ersingg77@gmail.com>
2025-10-08 11:36:15 +0300

    add wordlist

--- main.py
+++ main.py
@@ -2,19 +2,13 @@ import requests
 
 target_domain = "github.com"
 
-wordlist = [
-    "docs",
-    "status",
-    "yokkkk",
-    "api",
-    "kelime5",
-            ]
-
-for subdomain in wordlist:
-    url = f"https://{subdomain}.{target_domain}"
-    try:
-        requests.get(url)
-    except requests.exceptions.RequestException as e:
-        pass
-    else:
-        print(f"{url} bulundu")
+with open('wordlist.txt','r')as file:
+    for subdomain in file:
+        clear_string = subdomain.strip()
+        url = f"https://{clear_string}.{target_domain}"
+        try:
+            requests.get(url)
+        except requests.exceptions.RequestException as e:
+            pass
+        else:
+            print(f"{url} bulundu")
